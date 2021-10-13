2c38c00f8e7df80641b0c11ef07d416888addbd0
Daniel-Butt <daniel_b14@hotmail.ca>
2021-10-13 18:16:29 -0400

    updated script.js

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ let nextPlayer = 'X'; // takes a value of either 'X' or 'O' according to the gam
 //initialize the game
 
 // use the value stored in the nextPlayer variable to indicate who the next player is
+document.getElementById('next-lbl').innerText = nextPlayer;
 
 
 //This call will create the buttons needed for the gameboard.
@@ -11,7 +12,15 @@ createGameBoard()
 function createGameBoard()
 {
     // Programatically add a button with square brackets enclosing an empty space to each cell in the gameboard
-   
+    let gameboard = document.getElementById('gameboard').firstElementChild;
+
+    let rows = gameboard.children;
+
+    for(row of rows){
+        for(column of row.children){
+            column.innerHTML = `<button class='button'>[ ]</button>`;
+        }
+    }
 }
 
 // Programatically add 'takeCell' as an event listener to all the buttons on the board
@@ -29,18 +38,83 @@ function takeCell(event)
     */
 
     // Make sure the button is clickable only once (I didn't mention how to do that, look it up :) )
+    btn = event.target;
+    btn.innerText = '[' + nextPlayer + ']'
+    btn.disabled = true;
 
     // Check if the game is over
     if (isGameOver())
     {
         // let the lable with the id 'game-over-lbl' display the words 'Game Over' inside <h1> element
-    }
+        // I'll leave declaring the winner for your intrinsic motivation, it's not required for this assignment 
+        let btns = document.querySelectorAll('button');
+        let winner = 'n/a';
 
-    // I'll leave declaring the winner for your intrinsic motivation, it's not required for this assignment 
+        for(let i = 0; i < 9; i+= 3){
+            if(btns[i].innerText == btns[i+1].innerText && btns[i+1].innerText == btns[i+2].innerText){
+                console.log(btns[i].innerText + '1')
+                if(btns[i].innerText == "[X]"){
+                    winner = 'X';
+                }
+                else{
+                    winner = 'O';
+                }
+            }
+        }
+        for(let i = 0; i < 3; i++){
+            if(btns[i].innerText == btns[i+3].innerText && btns[i+3].innerText == btns[i+6].innerText){
+                console.log(btns[i].innerText + '2')
+                if(btns[i].innerText == "[X]"){
+                    winner = 'X';
+                }
+                else{
+                    winner = 'O';
+                }
+            }
+        }
+
+        if((btns[0].innerText == btns[4].innerText && btns[4].innerText == btns[8].innerText)){
+            console.log(btns[0].innerText + '3')
+            if(btns[0].innerText == "[X]"){
+                winner = 'X';
+            }
+            else{
+                winner = 'O';
+            }
+        }
+        else if((btns[2].innerText == btns[4].innerText && btns[4].innerText == btns[6].innerText)){
+            console.log(btns[3].innerText)
+            if(btns[3].innerText == "[X]" + '4'){
+                winner = 'X';
+            }
+            else{
+                winner = 'O';
+            }
+        }
+
+        document.getElementById('game-over-lbl').innerText = 'Game Over ' + winner + ' wins!';
+    }
+    else{
+        if(nextPlayer === 'X'){
+            nextPlayer = 'O';
+        }
+        else{
+            nextPlayer = 'X';
+        }
+        document.getElementById('next-lbl').innerText = nextPlayer;
+    }
 }
 
 function isGameOver()
 {
     // This function returns true if all the buttons are disabled and false otherwise 
-   
+    let btns = document.querySelectorAll('button');
+    for (let i=0; i<btns.length; i++)
+    {
+        if(!btns[i].disabled){
+            return false;
+        }
+            
+    }
+    return true;
 }
